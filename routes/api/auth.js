50468831f4104406d919a6ac28875ca7ca8c34b1
jsadowyj0 <sadowyjj@gmail.com>
2020-08-28 22:32:10 -0400

    Commented auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -23,32 +23,41 @@ router.post(
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
+    // Validate input with express-validator
     const errors = validationResult(req);
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
 
+    // Destructure name email and password from request body
     const { name, email, password } = req.body;
 
     try {
+      // Check to see if there is a user with that email
       const userMatch = await User.findOne({ email });
 
+      // if the user already exists return a 400
       if (userMatch)
         return res
           .status(400)
           .json({ errors: [{ msg: 'User already exists' }] });
 
+      // Hash password with 10 rounds
       const hashedPassword = await bcrypt.hash(password, 10);
 
+      // Create a new User
       const user = new User({ name, email, password: hashedPassword });
 
+      // Save user into the database
       await user.save();
 
+      // Create a new JWT token with userId stored in the payload
       const token = jwt.sign(
         { user: { id: user.id } },
         process.env.JWT_SECRET,
         { expiresIn: '5 days' }
       );
 
+      // Return 201 along with the JWT token
       res.status(201).json({ token });
     } catch (err) {
       throw err;
@@ -63,29 +72,37 @@ router.post(
   '/api/login',
   [
     check('email', 'Please enter a valid email.').isEmail(),
-    check('password').exists(),
+    check('password', 'Please enter a password.').exists(),
   ],
   async (req, res) => {
+    // Validate inputs with express-validator
     const errors = validationResult(req);
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
 
+    // Destructure email and password from request body
     const { email, password } = req.body;
 
     try {
+      // Find a user with matching email
       const user = await User.findOne({ email });
+
+      // if there is no match return a 400
       if (!user)
         return res
           .status(400)
           .json({ errors: [{ msg: 'Invalid Credentials' }] });
 
+      // compare hashed password and plain-text password
       const isMatch = await bcrypt.compare(password, user.password);
 
+      // if the passwords don't match a return a 400
       if (!isMatch)
         return res
           .status(400)
           .json({ errors: [{ msg: 'Invalid Credentials' }] });
 
+      // Create a new JWT with the userId as the payload
       const token = jwt.sign(
         { user: { id: user.id } },
         process.env.JWT_SECRET,
@@ -94,6 +111,7 @@ router.post(
         }
       );
 
+      // Respond with the token
       res.status(201).json({ token });
     } catch (err) {
       throw err;
@@ -106,9 +124,12 @@ router.post(
 // @access  Private
 router.get('/api/user', auth, async (req, res) => {
   try {
+    // Find a user that matches with request's userId
     const user = await User.findById(req.user.id).select('-password');
+    // if there is no user respond with a 404
     if (!user)
       return res.status(404).json({ errors: [{ msg: 'User not found' }] });
+    // Respond with user data
     res.json(user);
   } catch (err) {
     throw err;
