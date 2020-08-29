0d84c73c632f6e6fbd8d9528597e0185bd113cb0
jsadowyj0 <sadowyjj@gmail.com>
2020-08-28 22:20:31 -0400

    Added comments to todos.js

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -13,10 +13,13 @@ const router = express.Router();
 // @access  Private
 router.get('/api/todos', auth, async (req, res, next) => {
   try {
+    // Find all todos where user is equal to current user
     const todos = await Todo.find({ user: req.user.id });
 
+    // Return json of matching todos
     return res.json({ todos });
   } catch (err) {
+    // Pass error onto express error handling function
     next(err);
   }
 });
@@ -30,20 +33,26 @@ router.get(
   checkObjectId('id'),
   async (req, res, next) => {
     try {
+      // Destructure id property from request parameters
       const { id } = req.params;
 
+      // Find one todo with id from paramter
       const todo = await Todo.findById(id);
 
+      // if no todo is found then send a 404
       if (!todo)
         return res
           .status(404)
           .json({ errors: [{ msg: 'Not found', status: 404 }] });
 
+      // if todos userId does not match the requests userId return 401
+      // this is to make sure that a random person can't see your todos
       if (todo.user.toString() !== req.user.id.toString())
         return res
           .status(401)
           .json({ errors: [{ msg: 'Unauthorized', status: 401 }] });
 
+      // return todo json
       return res.json(todo);
     } catch (err) {
       next(err);
@@ -57,19 +66,24 @@ router.get(
 router.post(
   '/api/todos',
   auth,
-  check('content').isString(),
+  check('content').isString().isLength({ min: 1 }),
   async (req, res, next) => {
+    // check for any express-validator errors
     const errors = validationResult(req);
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
 
     try {
+      // Destructure content property from request body
       const { content } = req.body;
 
+      // Create a new todo with content and user (from auth middleware)
       const todo = new Todo({ content, user: req.user.id });
 
+      // Save todo into the database
       await todo.save();
 
+      // Return todo with status 201
       return res.status(201).json(todo);
     } catch (err) {
       next(err);
@@ -86,22 +100,28 @@ router.put(
   checkObjectId('id'),
   async (req, res, next) => {
     try {
+      // Destructure id from request parameters
       const { id } = req.params;
 
+      // Find the matching todo
       const todo = await Todo.findById(id);
 
+      // if no todo is found then return a 404
       if (!todo)
         return res
           .status(404)
           .json({ errors: [{ msg: 'Not found', status: 404 }] });
 
+      // if todos userId does not match the requests userId return 401
       if (todo.user.toString() !== req.user.id)
         return res
           .status(401)
           .json({ errors: [{ msg: 'Unauthorized', status: 401 }] });
 
+      // Destructure content and completed properties
       const { content, completed } = req.body;
 
+      // Handle the case of no content and no completed value specified
       if (!content && typeof completed === 'undefined')
         return res.status(400).json({
           errors: [
@@ -112,12 +132,16 @@ router.put(
           ],
         });
 
+      // if there is content specified modify it
       if (content) todo.content = content;
 
+      // if there is a completed value specified modify it
       if (typeof completed !== 'undefined') todo.completed = completed;
 
+      // Save updated todo into database
       await todo.save();
 
+      // Return todo with status 200
       return res.status(200).json(todo);
     } catch (err) {
       next(err);
@@ -133,23 +157,29 @@ router.delete(
   auth,
   checkObjectId('id'),
   async (req, res, next) => {
+    // Destructure id from request parameters
     const { id } = req.params;
 
     try {
+      // Try to find todo with a matching id
       const todo = await Todo.findById(id);
 
+      // if no todo is found return 404
       if (!todo)
         return res
           .status(404)
           .json({ errors: [{ msg: 'Not found', status: 404 }] });
 
+      // if users don't match return a 401
       if (todo.user.toString() !== req.user.id)
         return res
           .status(401)
           .json({ errors: [{ msg: 'Unauthorized', status: 401 }] });
 
+      // Remove todo from the database
       await todo.remove();
 
+      // Return a success message
       return res.json({ msg: 'Todo Removed', status: 200 });
     } catch (err) {
       next(err);
